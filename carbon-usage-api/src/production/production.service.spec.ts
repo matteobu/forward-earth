97a9ad7357e697bf4deb549b19c9a6bc038f3df0
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-04-06 11:23:06 +0200

    README file updated

--- carbon-usage-api/src/production/production.service.spec.ts
+++ carbon-usage-api/src/production/production.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductionService } from './production.service';
+import { SupabaseService } from '../supabase/supabase.service';
 
 describe('ProductionService', () => {
   let service: ProductionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductionService],
+      providers: [SupabaseService, ProductionService],
     }).compile();
 
     service = module.get<ProductionService>(ProductionService);
