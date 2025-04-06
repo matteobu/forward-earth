c85d33db7cb5213d2933b94fb69f6a72f1179fa1
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-04-06 12:45:17 +0200

    Updating Consumption fixed

--- carbon-usage-api/src/consumption/dto/update-consumption.dto.ts
+++ carbon-usage-api/src/consumption/dto/update-consumption.dto.ts
@@ -1,1 +1,27 @@
-export class UpdateConsumptionDto {}
+import { IsNumber, IsString, IsOptional } from 'class-validator';
+
+export class UpdateConsumptionDto {
+  @IsOptional()
+  @IsNumber()
+  amount?: number;
+
+  @IsOptional()
+  @IsNumber()
+  co2_equivalent?: number;
+
+  @IsOptional()
+  @IsString()
+  date?: string;
+
+  @IsOptional()
+  @IsNumber()
+  activity_type_table_id?: number;
+
+  @IsOptional()
+  @IsString()
+  unit?: string;
+
+  @IsOptional()
+  @IsNumber()
+  emission_factor?: number;
+}
